7318d9880ab13174582f2f1176982da38148d68a
BohdanStepanenko <stepanenko1905@gmail.com>
2022-11-22 23:15:39 +0200

    Create Result migration

---
+++ database/migrations/2022_11_22_202303_create_results.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('results', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->json('results')->nullable();
+            $table->unsignedInteger('summary')->nullable();
+            $table->timestamps();
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('results');
+    }
+};
